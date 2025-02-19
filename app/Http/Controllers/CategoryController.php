fd47cee463a86155985df55dd7123b8c9933b3c1
Mesut Yılmaz <mesutyilmaz1993@gmail.com>
2025-02-19 13:59:36 +0300

    Category model, controller, migration and api routes created.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function getAll()
+    {
+        //Todo
+    }
+
+    public function show()
+    {
+        //Todo
+    }
+
+    public function create()
+    {
+        //Todo
+    }
+
+    public function update()
+    {
+        //Todo
+    }
+
+    public function delete()
+    {
+        //Todo
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 
 Route::group(['prefix' => 'auth'], function ($router) {
     Route::post('register', [AuthController::class, 'register']);
@@ -10,5 +11,18 @@ Route::group(['prefix' => 'auth'], function ($router) {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
         Route::post('me', [AuthController::class, 'me']);
+    });
+});
+
+Route::group([
+    'prefix' => 'categories',
+    'controller' => CategoryController::class
+], function ($router) {
+    Route::get('/','getAll');
+    Route::post('/{id}', 'show');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', 'create');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'delete');
     });
 });
