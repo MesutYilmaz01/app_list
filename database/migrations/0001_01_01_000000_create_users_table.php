67b0648ab87e543813914feb18f4417730769c68
Mesut Yılmaz <mesutyilmaz1993@gmail.com>
2025-02-24 16:06:31 +0300

    Soft delete added to category & user models and migraitons.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->integer('user_type')->default(0);
+            $table->softDeletes(); 
             $table->rememberToken();
             $table->timestamps();
         });
