8a818c63b0be0bc804f4f386903f939d303f6169
Mesut Yılmaz <mesutyilmaz1993@gmail.com>
2025-04-10 19:47:18 +0300

    Authority requests and dto and entitiy created.

---
+++ app/Http/Requests/Admin/Authortiy/AuthorityDeleteRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Admin\Authority;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AuthorityDeleteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'authortiy_id' => ['required', 'exists:authorities,id'],
+        ];
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge(['authortiy_id' => $this->route('authortiy_id')]);
+    }
+}
---
+++ app/Modules/Authority/Domain/Entities/AuthorityEntity.php
@@ -0,0 +1,8 @@
+<?
+
+namespace App\Modules\Authority\Domain\Entities;
+
+use App\Models\Authortiy;
+use App\Modules\Shared\Interfaces\Entities\IEntity;
+
+class AuthorityEntity extends Authortiy implements IEntity {}
