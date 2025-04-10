a616abad5624e5680dbbc8d669007657540deea6
Mesut Yılmaz <mesutyilmaz1993@gmail.com>
2025-04-10 23:18:13 +0300

    Authority model name fixed.

--- app/Modules/Authority/Domain/Entities/AuthorityEntity.php
+++ app/Modules/Authority/Domain/Entities/AuthorityEntity.php
@@ -2,7 +2,7 @@
 
 namespace App\Modules\Authority\Domain\Entities;
 
-use App\Models\Authortiy;
+use App\Models\Authority;
 use App\Modules\Shared\Interfaces\Entities\IEntity;
 
-class AuthorityEntity extends Authortiy implements IEntity {}
+class AuthorityEntity extends Authority implements IEntity {}
